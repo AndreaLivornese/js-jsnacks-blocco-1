96d5244f0f3d92261f3bf03484113c637b912b78
andrea-livornese <andrealivornese@gmail.com>
2024-02-02 19:30:22 +0100

    Snack 4: creato struttura dello script

--- snack 4/script.js
+++ snack 4/script.js
@@ -1,3 +1,20 @@
 // Chiedi un numero di 4 cifre all’utente
 // e calcola la somma di tutte le cifre che compongono il numero.
 
+const input = prompt("inserisci un numero");
+
+console.log(input);
+
+let numeri=[];
+let somma=0;
+
+// numeri[0]=Number(input[0]);
+// console.log(numeri[0]);
+
+for(let i=0; i<input.length; i++){
+    numeri[i]=Number(input[i]);
+    somma+=numeri[i];
+}
+
+
+console.log(somma);
