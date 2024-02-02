83cf18ad33fa4049c19b843ed6f38e1cd89048aa
andrea-livornese <andrealivornese@gmail.com>
2024-02-02 19:38:05 +0100

    Snack 4: script completato e minimo di struttura html

--- snack 4/script.js
+++ snack 4/script.js
@@ -3,18 +3,22 @@
 
 const input = prompt("inserisci un numero");
 
-console.log(input);
+if(input.length > 4){
+    alert("Devi inserire un numero massimo di 4 cifre... riaggiorna la pagina per riprovare")
+}else{
+    
+    let numeri=[];
+    let somma=0;
+    
+    
+    document.querySelector("#in-number").innerText = "Il numero che hai inserito è: " + input;
+    
+    for(let i=0; i<input.length; i++){
+        numeri[i]=Number(input[i]);
+        somma+=numeri[i];
+    }
+    
+    
+    document.querySelector("#result").innerText = "La somma delle cifre del numero inserito è: " + somma;
 
-let numeri=[];
-let somma=0;
-
-// numeri[0]=Number(input[0]);
-// console.log(numeri[0]);
-
-for(let i=0; i<input.length; i++){
-    numeri[i]=Number(input[i]);
-    somma+=numeri[i];
 }
-
-
-console.log(somma);
