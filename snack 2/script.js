3d33c3ec42204cdccdabe044e309f9f480006832
andrea-livornese <andrealivornese@gmail.com>
2024-02-02 16:41:30 +0100

    Snack 2:creato scheletro dello script e struttura html

--- snack 2/script.js
+++ snack 2/script.js
@@ -3,3 +3,14 @@
 
 
 
+let numbers=[];
+let somma=0;
+
+for(let i=0; i<10; i++){
+    numbers[i]=Number(prompt("inserisci un numero"));
+    somma+=numbers[i];
+
+}
+
+console.log("somma: ", somma);
+
