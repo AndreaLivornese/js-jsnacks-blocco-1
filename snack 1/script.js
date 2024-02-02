8696b60eb64e3c3451c3aff2ceab2e28ba392a81
andrea-livornese <andrealivornese@gmail.com>
2024-02-02 16:29:20 +0100

    Snack 1: creato minimo di grafica, inseriti gli output nello script e fine snack

--- snack 1/script.js
+++ snack 1/script.js
@@ -2,18 +2,39 @@
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 
-let words;
+const longDivElement = document.querySelector("#long");
+const shortDivElement = document.querySelector("#short");
+const drawDivElement = document.querySelector("#draw");
 
+
+// array  di string
+let words=[];
+
+
+// input
 for(let i=0; i < 2; i++){
     words[i]=prompt("inserisci una parola");
 }
 
-if(words[0].lenght == words[1].lenght){
-    // output hanno la stessa lunghezza
-}else if(words[0].lenght == words[1].lenght){
+if(words[0].length == words[1].length){
+
+    drawDivElement.innerText = "le parole hanno la stessa lunghezza "
+
+}else if(words[0].length > words[1].length){
+
     // output words[0]
+    longDivElement.innerText += "La frase più lunga è: " + words[0];
+   
     // output words[1]
+    shortDivElement.innerText += "La parola più corta è: " + words[1];
+    
+
 }else{
-    // output words[1]
+
     // output words[0]
+    longDivElement.innerText += "La frase più lunga è: " + words[1];
+    
+    // output words[1]
+    shortDivElement.innerText += "La parola più corta è: " + words[0];
+   
 }
