b542804b35898c1e3c7a6e97b1ff5c03ce5a3a51
andrea-livornese <andrealivornese@gmail.com>
2024-02-02 16:07:51 +0100

    SNACK 1: compleatata struttura dello script

--- snack 1/script.js
+++ snack 1/script.js
@@ -1,3 +1,19 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
+
+let words;
+
+for(let i=0; i < 2; i++){
+    words[i]=prompt("inserisci una parola");
+}
+
+if(words[0].lenght == words[1].lenght){
+    // output hanno la stessa lunghezza
+}else if(words[0].lenght == words[1].lenght){
+    // output words[0]
+    // output words[1]
+}else{
+    // output words[1]
+    // output words[0]
+}
