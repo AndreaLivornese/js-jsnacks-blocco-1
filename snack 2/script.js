afcf211f784867185038ccb3b00fae07808aad09
andrea-livornese <andrealivornese@gmail.com>
2024-02-02 16:53:15 +0100

    Snack 2: completata struttura html, fine script e fine scank 2

--- snack 2/script.js
+++ snack 2/script.js
@@ -2,15 +2,24 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 
-
+const olElement = document.querySelector("ol");
+let liElement;
 let numbers=[];
 let somma=0;
 
 for(let i=0; i<10; i++){
+    
     numbers[i]=Number(prompt("inserisci un numero"));
+
+    liElement= document.createElement("li");
+
+    liElement.innerText= numbers[i];
+
+    olElement.append(liElement);
+
     somma+=numbers[i];
 
 }
 
-console.log("somma: ", somma);
+document.querySelector("#somma").innerText = "la somma dei numeri è: " + somma;
 
